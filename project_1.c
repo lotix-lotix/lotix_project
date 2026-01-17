8f026f073c296a8e416248417df4fac4cae8170c
lotix <dima.vertelkin03@mail.ru>
2026-01-17 14:05:42 +0300

    a

--- project_1.c
+++ project_1.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pull_stats(char* name, short old, short hight, short wight){
+typedef union{
+        unsigned char buffer_c[8];
+        unsigned short buffer_s[2];
+        unsigned buffer_i;
+    }swap_buffer;
+
+typedef struct {
+        swap_buffer name;
+        swap_buffer old;
+        swap_buffer hight;
+        swap_buffer wight;
+    } STATS;
+
+void set_stats(STATS* arr);
+
+void pull_stats(STATS* name){
     char ch = 0;
     int i = 0;
-    char* hig = malloc(1 * sizeof(hight));
-    char* wig = malloc(1 * sizeof(wight));
+    //char* hig = malloc(1 * sizeof(hight));
+    //char* wig = malloc(1 * sizeof(wight));
     printf("Enetr you name: ");
-    while(((ch = getchar()) != 0) && ch != '\n' && i < sizeof(name)){
-        name[i] = ch;
-        i++;
-    }
+    set_stats(name);
 
     i = 0;
     ch = 0; 
@@ -18,44 +30,46 @@ void pull_stats(char* name, short old, short hight, short wight){
     //printf("Enetr you old: ");
     //scanf("%d", &old);
 
-    printf("Enetr you hight: ");
+    /*printf("Enetr you hight: ");
     while(((ch = getchar()) != 0) && ch != '\n' && i < sizeof(hight)){
         hig[i] = ch;
         i++;
         if(ch == '\n')
             break;
-    }
+    }*/
 
-    hight = atoi(hig);
+    //hight = atoi(hig);
     i = 0;
     ch = 0; 
 
-    printf("Enetr you wight: ");
+    /*printf("Enetr you wight: ");
     while(((ch = getchar()) != 0) && ch != '\n' && i < sizeof(wight)){
         wig[i] = ch;
         i++;
         if(ch == '\n')
             break;
+    }*/
+
+    //wight = atoi(wig);
+
+    //free(wig);
+    //free(hig);
+}
+
+void set_stats(STATS* nun){
+    
+    for(int i = 0; i < (sizeof(nun) / sizeof(nun)); i++){
+        nun->name.buffer_c[i] = getchar();
+        if(nun->name.buffer_c == '\n')
+            break;
     }
-
-    wight = atoi(wig);
-
-    free(wig);
-    free(hig);
 }
 
 int main(){
 
-    typedef struct {
-        char name[10];
-        unsigned short old;
-        unsigned short hight;
-        unsigned short wight;
-    } STATS;
-
     STATS people;
-    pull_stats(people.name, people.old, people.hight, people.wight);
-    printf("Name: %s, Old: %d, Hight: %d, Wight: %d",people.name, people.old, people.hight, people.wight);
+    pull_stats(people.name.buffer_c);
+    printf("Name: %s",people.name);
 
     return 0;
 }
---
+++ ykazatel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int arg = 4;
+    int* ptr_arg = &arg;
+    char* ptr_arg_bit = (char*)ptr_arg;
+
+    *ptr_arg = 100;
+
+    printf("*ptr = %d, *ptr_arg_bit = %c, arg = %d\n\n", *ptr_arg, *ptr_arg_bit, arg);
+
+    ptr_arg_bit += 2;
+    *ptr_arg_bit = 3;
+
+    printf("*ptr = %d, *ptr_arg_bit = %c, arg = %d", *ptr_arg, *ptr_arg_bit, arg);
+    
+    
+    return 0;
+}
